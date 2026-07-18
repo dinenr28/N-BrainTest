d3d6425cfc6a6ca92854c94e4fa663b4b0018839
dinenr28 <nadinenareshwarir@gmail.com>
2026-07-18 21:07:44 +0700

    Create kimia.py

---
+++ database/kimia.py
@@ -0,0 +1,24 @@
+KIMIA_DATA = {
+    "SMP": [],
+    "SMA": [
+        {
+            "id": "kim-sma-01",
+            "judul": "Struktur Atom",
+            "isi_konten": "Elektron bermuatan negatif, proton positif, dan neutron netral.",
+            "soal_latihan": [
+                {
+                    "id": "kim-sma-q1",
+                    "tipe": "essay",
+                    "pertanyaan": "Apakah nama partikel atom bermuatan negatif?",
+                    "kunci_jawaban": "elektron",
+                    "pembahasan": "Partikel bermuatan negatif adalah elektron."
+                }
+            ]
+        }
+    ]
+}
+
+PRE_POST_KIMIA = {
+    "pre_test": {"SMP": [], "SMA": []},
+    "post_test": {"SMP": [], "SMA": []}
+}
